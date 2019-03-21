beb6fbbd87ed2b9f9cd347caa26522e19d3bcb7f
滨边家的小美波 <3096237774@qq.com>
2019-03-21 22:08:15 +0800

    路由配置

--- server.js
+++ server.js
@@ -2,6 +2,29 @@
 var express = require('express');   //引入express
 var app = express();
 app.use(express.static(__dirname + '/public'));
+
+//路由部分
+app.get('/contactList',function(req,res){
+    console.log("T received a get request");
+    person1 = {
+        name:'tim',
+        email:'tim@email.com',
+        numberL:123456
+    };
+    person2 = {
+        name:'Emaily',
+        email:'Emaily@email.com',
+        numberL:111111
+    };
+    person3 = {
+        name:'Jhone',
+        email:'Jhone@email.com',
+        numberL:888888
+    }
+    var contactList = [person1,person2,person3];
+    res.json(contactList);
+});
+
 app.listen(3000);   //监听3000端口
 console.log("server running on port 3000"); //提示运行成功
 
