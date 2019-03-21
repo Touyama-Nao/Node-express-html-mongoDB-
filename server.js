8661df3041bb0be01d0582ff6f60884f230fef74
滨边家的小美波 <3096237774@qq.com>
2019-03-21 23:19:17 +0800

    修改

--- server.js
+++ server.js
@@ -9,47 +9,62 @@ app.use(express.static(__dirname + '/public'));
 
 //路由部分
 app.get('/contactList', function (req, res) {
-    console.log("I received a get request");
-    db.contactList.find(function (err, docs) {
-        console.log(docs);
-        res.json(docs);
-    })
-/*     person1 = {
-        name: 'tim',
-        email: 'tim@email.com',
-        numberL: 123456
-    };
-    person2 = {
-        name: 'Emaily',
-        email: 'Emaily@email.com',
-        numberL: 111111
-    };
-    person3 = {
-        name: 'Jhone',
-        email: 'Jhone@email.com',
-        numberL: 888888
-    }
-    var contactList = [person1, person2, person3];
-    res.json(contactList); //请求后返回一个json对象 */
+	console.log("I received a get request");
+	db.contactList.find(function (err, docs) {
+		console.log(docs);
+		res.json(docs);
+	})
+	/*     person1 = {
+	        name: 'tim',
+	        email: 'tim@email.com',
+	        numberL: 123456
+	    };
+	    person2 = {
+	        name: 'Emaily',
+	        email: 'Emaily@email.com',
+	        numberL: 111111
+	    };
+	    person3 = {
+	        name: 'Jhone',
+	        email: 'Jhone@email.com',
+	        numberL: 888888
+	    }
+	    var contactList = [person1, person2, person3];
+	    res.json(contactList); //请求后返回一个json对象 */
 });
-app.post(function (req, res) {  //插入记录
-    console.log("this is a post request");
-    console.log(req.body);
-    db.contactList.insert(req.body,function(err,doc){   //用到了req.body,所以必须要引入body-parser模块
-        res.json(doc);
-    })
+app.post(function (req, res) { //插入记录
+	console.log("this is a post request");
+	console.log(req.body);
+	db.contactList.insert(req.body, function (err, doc) { //用到了req.body,所以必须要引入body-parser模块
+		res.json(doc);
+	})
 })
 
-app.delete('contactlist/:id',function(req,res){ //删除路由
-    var id = req.params.id;
-    console.log(id);
-    db.contactList.remove({_id:mongojs.ObjectId(id)},function(err,res){
-        res.json(res);
-    })
+app.delete('contactlist/:id', function (req, res) { //删除路由
+	var id = req.params.id;
+	console.log(id);
+	db.contactList.remove({
+		_id: mongojs.ObjectId(id)
+	}, function (err, res) {
+		res.json(res);
+
+	})
 })
 
+//数据库的更新操作
+app.put('/contactlist/:id', function (req, res) {
+			var id = req.params.id;
+			console.log(req.body.name);
+			db.contactList.findAndModify({
+					query: {
+						id: mongojs.ObjectId(id)
+					},updata:{
+						$set: {name:req.body.name,email:req.body.email,number:req.body.number},
+					},
+			new:true},function(err,doc){
+				res.json(doc)
+			})
 
 
-
-app.listen(3000); //监听3000端口
-console.log("server running on port 3000"); //提示运行成功
+		app.listen(3000); //监听3000端口
+		console.log("server running on port 3000"); //提示运行成功
